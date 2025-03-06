559828b861c4ac6d0cfe9f7c340908f45ec9a2c8
Rebeca <becalbuquerque_23@hotmail.com>
2025-03-06 18:42:24 -0300

    testanto

---
+++ src/test/java/com/projeto/projetolistacompras/ProjetolistacomprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.projetolistacompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetolistacomprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

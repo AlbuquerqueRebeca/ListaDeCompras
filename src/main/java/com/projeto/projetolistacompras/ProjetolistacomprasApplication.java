e3a61b418a26607830f3f88270dfddcc5ccb8a85
Rebeca <becalbuquerque_23@hotmail.com>
2025-03-06 18:42:24 -0300

    testanto

---
+++ src/main/java/com/projeto/projetolistacompras/ProjetolistacomprasApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.projetolistacompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetolistacomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetolistacomprasApplication.class, args);
+	}
+
+}

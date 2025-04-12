371dcda91b726f15be85f37563409e911ca04773
Rebeca <becalbuquerque_23@hotmail.com>
2025-04-12 18:01:26 -0300

    adicionando a anotaçap id

---
+++ src/main/java/com/projeto/projetolistacompras/Repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.projetolistacompras.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.projetolistacompras.Entidade.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}

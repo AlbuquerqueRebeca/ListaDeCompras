e395c2dd3d9c62ddde767faa45e8293ba764e5c1
Rebeca <becalbuquerque_23@hotmail.com>
2025-04-12 18:37:20 -0300

    adicionando a anotaçao generatedvalue

--- src/main/java/com/projeto/projetolistacompras/Entidade/ListaDeCompras.java
+++ src/main/java/com/projeto/projetolistacompras/Entidade/ListaDeCompras.java
@@ -1,12 +1,14 @@
 package com.projeto.projetolistacompras.Entidade;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity 
 public class ListaDeCompras {
 	
-@Id	
+@Id
+@GeneratedValue()
 
 	
 	

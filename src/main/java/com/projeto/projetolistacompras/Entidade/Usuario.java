94366928910b85d1d94d80a123d54cbe471fa4bd
Rebeca <becalbuquerque_23@hotmail.com>
2025-04-12 18:30:56 -0300

    Adicionando getters e setters

--- src/main/java/com/projeto/projetolistacompras/Entidade/Usuario.java
+++ src/main/java/com/projeto/projetolistacompras/Entidade/Usuario.java
@@ -36,7 +36,42 @@ public Usuario() {
 		this.senha = senha; 
 		
 	}
+	
+	//getters / setters 
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public List<ListaDeCompras> getListaDeCompras() {
+		return listaDeCompras;
+	}
+	public void setListaDeCompras(List<ListaDeCompras> listaDeCompras) {
+		this.listaDeCompras = listaDeCompras;
+	}
 
+	
+	
 
 
 

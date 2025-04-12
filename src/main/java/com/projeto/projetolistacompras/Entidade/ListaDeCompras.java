1ff9ed14563018d30f77c23136693f7954843f0e
Rebeca <becalbuquerque_23@hotmail.com>
2025-04-12 18:35:09 -0300

    adicionando a anotaçao entity

--- src/main/java/com/projeto/projetolistacompras/Entidade/ListaDeCompras.java
+++ src/main/java/com/projeto/projetolistacompras/Entidade/ListaDeCompras.java
@@ -1,8 +1,8 @@
 package com.projeto.projetolistacompras.Entidade;
 
+import jakarta.persistence.Entity;
 
-
-
+@Entity 
 public class ListaDeCompras {
 
 	
--- src/main/java/com/projeto/projetolistacompras/Repository/UsuarioRepository.java
+++ src/main/java/com/projeto/projetolistacompras/Repository/UsuarioRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.projetolistacompras.Entidade.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> 
+
+ {
 
 }

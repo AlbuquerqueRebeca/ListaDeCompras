6887c0ebf9bf0eda3e57ae8e87112ea513940654
Rebeca <becalbuquerque_23@hotmail.com>
2025-04-12 18:14:47 -0300

    adicionando a anotação onetomany

--- src/main/java/com/projeto/projetolistacompras/Entidade/Usuario.java
+++ src/main/java/com/projeto/projetolistacompras/Entidade/Usuario.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity  
 public class Usuario {
@@ -18,6 +19,9 @@ private String nome;
 @Column(unique = true) 
 private String email; 
 private String senha; 
+
+
+@OneToMany
 
 	
 	

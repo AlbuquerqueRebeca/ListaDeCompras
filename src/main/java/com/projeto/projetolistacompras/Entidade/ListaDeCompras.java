80295b23658ec2003e51cc2a6bfba210e273a905
Rebeca <becalbuquerque_23@hotmail.com>
2025-04-12 18:45:01 -0300

    adicionando a anotaçao joincolumn

--- src/main/java/com/projeto/projetolistacompras/Entidade/ListaDeCompras.java
+++ src/main/java/com/projeto/projetolistacompras/Entidade/ListaDeCompras.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity 
@@ -16,7 +17,8 @@ private long id;
 
 private String nomeLista; 
 
-@ManyToOne 
+@ManyToOne
+@JoinColumn
 
 	
 	

73f281926a453b061b820fc02ab8556dcbce1509
Rebeca <becalbuquerque_23@hotmail.com>
2025-05-10 19:02:16 -0300

    Adicionando o metodo FindByusuarioId na interface

--- src/main/java/com/projeto/projetolistacompras/Repository/ListaDeComprasRepository.java
+++ src/main/java/com/projeto/projetolistacompras/Repository/ListaDeComprasRepository.java
@@ -1,9 +1,14 @@
 package com.projeto.projetolistacompras.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.projetolistacompras.Entidade.ListaDeCompras;
 
 public interface ListaDeComprasRepository extends JpaRepository<ListaDeCompras, Long>{   
+	
+	
+	List<ListaDeCompras> findByUsuarioId();
 
 }

2fd23743537ca55841d4e6487a3f5c70fd0f623c
Rebeca <becalbuquerque_23@hotmail.com>
2025-05-10 18:51:37 -0300

    Adicionando parametro no jpaRepository

--- src/main/java/com/projeto/projetolistacompras/Repository/ListaDeComprasRepository.java
+++ src/main/java/com/projeto/projetolistacompras/Repository/ListaDeComprasRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.projetolistacompras.Entidade.ListaDeCompras;
 
-public interface ListaDeComprasRepository extends JpaRepository<>{  
+public interface ListaDeComprasRepository extends JpaRepository<ListaDeCompras, Long>{   
 
 }

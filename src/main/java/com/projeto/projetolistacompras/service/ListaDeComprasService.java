216295334d9b2889b32d25ea0012e8a966d831ca
Rebeca <becalbuquerque_23@hotmail.com>
2025-05-10 18:53:34 -0300

    Adicionando injeçao de dependencia ListaDeComprasRepository

--- src/main/java/com/projeto/projetolistacompras/service/ListaDeComprasService.java
+++ src/main/java/com/projeto/projetolistacompras/service/ListaDeComprasService.java
@@ -2,7 +2,11 @@ package com.projeto.projetolistacompras.service;
 
 import org.springframework.stereotype.Service;
 
+import com.projeto.projetolistacompras.Repository.ListaDeComprasRepository;
+
 @Service
 public class ListaDeComprasService {
+	
+	private final ListaDeComprasRepository listaDeComprasRepository; 
 
 }

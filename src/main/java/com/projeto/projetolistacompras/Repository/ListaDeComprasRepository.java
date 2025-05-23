fe490a14765f3b447f813eeeb67d6139c0eb871a
Rebeca <becalbuquerque_23@hotmail.com>
2025-05-23 12:15:17 -0300

    adicionando o atributo listaDeCompras no repository

--- src/main/java/com/projeto/projetolistacompras/Repository/ListaDeComprasRepository.java
+++ src/main/java/com/projeto/projetolistacompras/Repository/ListaDeComprasRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.projetolistacompras.Entidade.ListaDeCompras;
 
-public interface ListaDeComprasRepository extends JpaRepository<ListaDeCompras, Long>{   
-	
-	
-	List<ListaDeCompras> findByUsuarioId(Long usuarioId); 
-
+public interface ListaDeComprasRepository extends JpaRepository<ListaDeCompras, Long>{
+    List<ListaDeCompras> findByUsuarioId(Long UsuarioId);
 }
--- src/main/java/com/projeto/projetolistacompras/Repository/UsuarioRepository.java
+++ src/main/java/com/projeto/projetolistacompras/Repository/UsuarioRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.projetolistacompras.Entidade.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> 
-
- {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }

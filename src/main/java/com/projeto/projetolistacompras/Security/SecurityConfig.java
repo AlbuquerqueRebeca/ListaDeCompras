a56b6f16b1b5172da3b2b191158ce83041314a8c
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-01 18:53:14 -0300

    adicionando o metodo authenticated

--- src/main/java/com/projeto/projetolistacompras/Security/SecurityConfig.java
+++ src/main/java/com/projeto/projetolistacompras/Security/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 		                              .csrf(csrf -> csrf.disable())
 		                              .authorizeHttpRequests(auth -> auth
 		                              .requestMatchers("/criar-usuario").permitAll()
-		                              .anyRequest()
+		                              .anyRequest().authenticated()
 		                              
 		
 	}

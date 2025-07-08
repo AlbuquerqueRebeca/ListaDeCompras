36359ad7e94c7429c20288665339d1a1e08f66e5
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-08 19:37:09 -0300

    adicionando a anotaçao postmapping na classe usuariocontroller

--- src/main/java/com/projeto/projetolistacompras/Controller/UsuarioController.java
+++ src/main/java/com/projeto/projetolistacompras/Controller/UsuarioController.java
@@ -1,7 +1,7 @@
 package com.projeto.projetolistacompras.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +18,11 @@ public class UsuarioController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository; 
+	
+	
+	
+	//criando novo usuario
+	@PostMapping
 	
 	
 	

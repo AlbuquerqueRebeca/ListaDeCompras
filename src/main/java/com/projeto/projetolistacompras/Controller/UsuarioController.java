07b5fe31848a876000e6c0eeaee4c12712dd2f0e
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-08 19:35:46 -0300

    implementando injeçao de dependencia da classe usuarioRepository

--- src/main/java/com/projeto/projetolistacompras/Controller/UsuarioController.java
+++ src/main/java/com/projeto/projetolistacompras/Controller/UsuarioController.java
@@ -1,9 +1,11 @@
 package com.projeto.projetolistacompras.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projeto.projetolistacompras.Repository.UsuarioRepository;
 import com.projeto.projetolistacompras.service.UsuarioService;
 
 @RestController 
@@ -13,6 +15,10 @@ public class UsuarioController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository; 
+	
 	
 	
 	

58c1315b8a192e5c009572dbc5feb315fd3f9f48
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-09 19:26:05 -0300

    criando um optional do tipo string na classe usuarioRepository

--- src/main/java/com/projeto/projetolistacompras/Repository/UsuarioRepository.java
+++ src/main/java/com/projeto/projetolistacompras/Repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package com.projeto.projetolistacompras.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.projetolistacompras.Entidade.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    
+	 Optional<String>
 }

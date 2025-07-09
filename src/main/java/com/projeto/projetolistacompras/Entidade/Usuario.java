f6c3dba00312bfff67c6c865a1a4f0b94e1d84f1
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-09 19:19:53 -0300

    Alterando o getnome

--- src/main/java/com/projeto/projetolistacompras/Entidade/Usuario.java
+++ src/main/java/com/projeto/projetolistacompras/Entidade/Usuario.java
@@ -42,15 +42,14 @@ public Usuario() {
 	
 	//getters / setters 
 	
-	public long getId() {
-		return id;
+	public String getNome() {
+		return nome;
 	}
 	public void setId(long id) {
 		this.id = id;
 	}
-	public String getNome() {
-		return nome;
-	}
+	
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}

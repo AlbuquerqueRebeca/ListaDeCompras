497400b6db563b2aeaf97a9b1537114f59b67666
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-14 18:40:38 -0300

    salvando dados de novo usuario

--- src/main/java/com/projeto/projetolistacompras/Controller/UsuarioController.java
+++ src/main/java/com/projeto/projetolistacompras/Controller/UsuarioController.java
@@ -32,7 +32,7 @@ public class UsuarioController{
 	//Rota publica
 	@PostMapping("/usuarios/criar-usuario")
 	public ResponseEntity<String> criarUsuario(@RequestBody Usuario usuario){
-		
+	usuarioService.salvar(usuario);
 	
 		
 

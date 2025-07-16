efbe06da68293d017903f8cce5290a8f6793bc80
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-16 18:13:41 -0300

    implementando injeçao de dependencia listadecomprasservice na classe listadecomprasController

--- src/main/java/com/projeto/projetolistacompras/Controller/ListaDeComprasController.java
+++ src/main/java/com/projeto/projetolistacompras/Controller/ListaDeComprasController.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projeto.projetolistacompras.service.ListaDeComprasService;
+
 @RestController
 @RequestMapping
 public class ListaDeComprasController {
 
 	@Autowired
+	private ListaDeComprasService listaDeComprasService;
 	
 	
 	

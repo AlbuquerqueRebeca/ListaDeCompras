41393950dba3f36606863471495f13aaffa29e50
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-16 18:55:06 -0300

    adicionando anotaçao pathvariable nos parametros do metodo buscarporEmail

--- src/main/java/com/projeto/projetolistacompras/Controller/ListaDeComprasController.java
+++ src/main/java/com/projeto/projetolistacompras/Controller/ListaDeComprasController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +31,7 @@ public class ListaDeComprasController {
 	
 	//Buscando lista por email
 	@GetMapping("/buscar/{email}")
-	public ResponseEntity<List<ListaDeCompras>>
+	public ResponseEntity<List<ListaDeCompras>> buscarPorEmail(@PathVariable)
 	
 	
 	

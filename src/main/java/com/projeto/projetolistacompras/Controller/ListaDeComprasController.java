5613a7a93cd931a3d74e4af43545f2aa6287c018
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-16 18:08:30 -0300

    adicionando anotaçao restController na classe listadecomprasController

--- src/main/java/com/projeto/projetolistacompras/Controller/ListaDeComprasController.java
+++ src/main/java/com/projeto/projetolistacompras/Controller/ListaDeComprasController.java
@@ -1,5 +1,14 @@
 package com.projeto.projetolistacompras.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ListaDeComprasController {
+	
+	
+	
+	
 
 }

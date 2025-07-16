0af27429dd0340ef64afe6a88df08f2175891c7d
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-16 18:58:39 -0300

    adicionando a busca por listas

--- src/main/java/com/projeto/projetolistacompras/Controller/ListaDeComprasController.java
+++ src/main/java/com/projeto/projetolistacompras/Controller/ListaDeComprasController.java
@@ -1,5 +1,7 @@
 package com.projeto.projetolistacompras.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projeto.projetolistacompras.Entidade.ListaDeCompras;
 import com.projeto.projetolistacompras.service.ListaDeComprasService;
 
 @RestController
@@ -31,7 +34,9 @@ public class ListaDeComprasController {
 	
 	//Buscando lista por email
 	@GetMapping("/buscar/{email}")
-	public ResponseEntity<List<ListaDeCompras>> buscarPorEmail(@PathVariable)
+	public ResponseEntity<List<ListaDeCompras>> buscarPorEmail(@PathVariable String email){
+		List<ListaDeCompras> listas = listaDeComprasService.buscarPorEmail(email);
+	}
 	
 	
 	

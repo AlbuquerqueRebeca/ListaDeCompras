ef28eed85f6aa04f67bc573f3604a5afdb7b592a
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-22 18:48:34 -0300

    adicionando anotaçao requestParam

--- src/main/java/com/projeto/projetolistacompras/Controller/ListaDeComprasController.java
+++ src/main/java/com/projeto/projetolistacompras/Controller/ListaDeComprasController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.projetolistacompras.Entidade.ListaDeCompras;
@@ -51,6 +52,6 @@ public class ListaDeComprasController {
 	
 	//Excluir lista
 	@DeleteMapping("/deletar-lista")
-	public ResponseEntity<String> excluindoLista()
+	public ResponseEntity<String> excluindoLista(@RequestParam)
 
 }

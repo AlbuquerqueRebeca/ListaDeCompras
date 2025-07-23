e858a225e3c85106374c48bc26b18f43eaaf2fcc
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-23 18:27:59 -0300

    adicionando anotaçao value na classe jwtutil

--- src/main/java/com/projeto/projetolistacompras/Jwt/JwtUtil.java
+++ src/main/java/com/projeto/projetolistacompras/Jwt/JwtUtil.java
@@ -1,5 +1,12 @@
 package com.projeto.projetolistacompras.Jwt;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtil {
+	
+	@Value
+	
 
 }

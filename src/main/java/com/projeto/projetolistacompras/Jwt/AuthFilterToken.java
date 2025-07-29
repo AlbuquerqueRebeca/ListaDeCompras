836bfb80f7b794a4acc7d0d3adb2f6f60e540441
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-29 18:49:19 -0300

    adicionando anotaçao autowired

--- src/main/java/com/projeto/projetolistacompras/Jwt/AuthFilterToken.java
+++ src/main/java/com/projeto/projetolistacompras/Jwt/AuthFilterToken.java
@@ -1,9 +1,12 @@
 package com.projeto.projetolistacompras.Jwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class AuthFilterToken extends OncePerRequestFilter{
 	
 	
+	   
+	  @Autowired
 
 }

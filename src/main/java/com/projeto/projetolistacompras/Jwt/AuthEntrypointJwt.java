06759ec579f383c0a5399d1a3f8814f21308260a
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-30 19:12:57 -0300

    implementando authenticationEntrypoint

--- src/main/java/com/projeto/projetolistacompras/Jwt/AuthEntrypointJwt.java
+++ src/main/java/com/projeto/projetolistacompras/Jwt/AuthEntrypointJwt.java
@@ -1,8 +1,9 @@
 package com.projeto.projetolistacompras.Jwt;
 
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AuthEntrypointJwt {
+public class AuthEntrypointJwt implements AuthenticationEntryPoint {
 
 }

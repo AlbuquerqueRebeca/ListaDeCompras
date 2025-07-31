16a4cf6c75ce528fca2011e7ee4816845dbc0826
Rebeca <becalbuquerque_23@hotmail.com>
2025-07-31 18:54:10 -0300

    adicionando construtor de usuarioDetailsImpl

--- src/main/java/com/projeto/projetolistacompras/service/UsuarioDetailsImpl.java
+++ src/main/java/com/projeto/projetolistacompras/service/UsuarioDetailsImpl.java
@@ -1,5 +1,8 @@
 package com.projeto.projetolistacompras.service;
 
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class UsuarioDetailsImpl implements UserDetails{
@@ -10,4 +13,13 @@ public class UsuarioDetailsImpl implements UserDetails{
 	     private String nome;
          private String username;
          private String password;
+         
+         
+         
+         
+         
+         public UsuarioDetailsImpl(String email, String nome, String username, String password,
+        		Collection<? extends GrantedAuthority> authorities) {
+        	 
+         }
 }

63fe27a669d48e554c86a7adad720ed2c7b18fb7
Rebeca <becalbuquerque_23@hotmail.com>
2025-08-05 18:25:10 -0300

    lançando uma exceção

--- src/main/java/com/projeto/projetolistacompras/service/UsuarioDetailsServiceImpl.java
+++ src/main/java/com/projeto/projetolistacompras/service/UsuarioDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.projeto.projetolistacompras.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.projeto.projetolistacompras.Repository.UsuarioRepository;
 
@@ -16,6 +17,8 @@ public class UsuarioDetailsServiceImpl implements UserDetailsService{
 	
 	
 	@Autowired
-	public UserDetails loadUserByUsername(String username)
+	public UserDetails loadUserByUsername(String username)throws UsernameNotFoundException{
+		
+	}
 
 }

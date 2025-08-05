0ef1f1bd21d3b745fb0ccb6caaa2ccc4d14da139
Rebeca <becalbuquerque_23@hotmail.com>
2025-08-05 18:19:20 -0300

    adicionando anotaçao autowired

--- src/main/java/com/projeto/projetolistacompras/service/UsuarioDetailsServiceImpl.java
+++ src/main/java/com/projeto/projetolistacompras/service/UsuarioDetailsServiceImpl.java
@@ -1,9 +1,12 @@
 package com.projeto.projetolistacompras.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class UsuarioDetailsServiceImpl implements UserDetailsService{
 	
 	
+	
+	@Autowired
 
 }

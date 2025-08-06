dc8d8a4c7c2a5235902fbcbf18b899b9e432d823
Rebeca <becalbuquerque_23@hotmail.com>
2025-08-06 18:10:54 -0300

    adicionando anotaçao autowired

--- src/main/java/com/projeto/projetolistacompras/service/AuthService.java
+++ src/main/java/com/projeto/projetolistacompras/service/AuthService.java
@@ -1,10 +1,12 @@
 package com.projeto.projetolistacompras.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 	
+	@Autowired
 	
 	
 

69de2e00f7de29600ba1da022d708d070da531e0
Rebeca <becalbuquerque_23@hotmail.com>
2025-08-26 11:42:22 -0300

    injetando authservice na classe authcontoller

--- src/main/java/com/projeto/projetolistacompras/Controller/AuthController.java
+++ src/main/java/com/projeto/projetolistacompras/Controller/AuthController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projeto.projetolistacompras.service.AuthService;
+
 @RestController
 @RequestMapping
 public class AuthController {
@@ -11,5 +13,6 @@ public class AuthController {
 	
 	
 	@Autowired
+	private AuthService authService;
 
 }

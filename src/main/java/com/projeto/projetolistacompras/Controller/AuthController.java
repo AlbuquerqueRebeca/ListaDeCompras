d8c49b5e6bde1fd00982f5bbf936ab8d42c59025
Rebeca <becalbuquerque_23@hotmail.com>
2025-08-26 11:49:12 -0300

    adicionando parametros ao login

--- src/main/java/com/projeto/projetolistacompras/Controller/AuthController.java
+++ src/main/java/com/projeto/projetolistacompras/Controller/AuthController.java
@@ -3,9 +3,11 @@ package com.projeto.projetolistacompras.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projeto.projetolistacompras.Dto.AuthenticationDto;
 import com.projeto.projetolistacompras.service.AuthService;
 import com.projeto.projetolistacompras.service.UsuarioService;
 
@@ -25,7 +27,9 @@ public class AuthController {
 	
 	
 	@PostMapping(value = "/login")
-	public ResponseEntity<?>  login()
+	public ResponseEntity<?>  login(@RequestBody AuthenticationDto authDto){
+		
+	}
 	
 
 }

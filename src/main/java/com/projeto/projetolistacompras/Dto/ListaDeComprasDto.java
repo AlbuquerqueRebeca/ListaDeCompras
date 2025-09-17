fb61dcfa12d35371fd467db68647d9455d6a8643
Rebeca <becalbuquerque_23@hotmail.com>
2025-09-17 19:07:10 -0300

    adicionando getters e setters

--- src/main/java/com/projeto/projetolistacompras/Dto/ListaDeComprasDto.java
+++ src/main/java/com/projeto/projetolistacompras/Dto/ListaDeComprasDto.java
@@ -7,5 +7,37 @@ public class ListaDeComprasDto {
 	private String nome;
 	private String descriçao;
 	private List<ItemDto> itens;
+	
+	
+	
+	
+	
+	
+	///getters e setters
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescriçao() {
+		return descriçao;
+	}
+	public void setDescriçao(String descriçao) {
+		this.descriçao = descriçao;
+	}
+	public List<ItemDto> getItens() {
+		return itens;
+	}
+	public void setItens(List<ItemDto> itens) {
+		this.itens = itens;
+	}
+	
+	
+	
+	
+	
+	
 
 }

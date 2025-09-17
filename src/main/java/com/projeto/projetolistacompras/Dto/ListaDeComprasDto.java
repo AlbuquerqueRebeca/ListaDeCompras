8ff676e46f54a1752d82721fcd75a6e73b9a1d78
Rebeca <becalbuquerque_23@hotmail.com>
2025-09-17 19:05:11 -0300

    adicionando a lista de itens

--- src/main/java/com/projeto/projetolistacompras/Dto/ListaDeComprasDto.java
+++ src/main/java/com/projeto/projetolistacompras/Dto/ListaDeComprasDto.java
@@ -1,8 +1,11 @@
 package com.projeto.projetolistacompras.Dto;
 
+import java.util.List;
+
 public class ListaDeComprasDto {
 	
 	private String nome;
 	private String descriçao;
+	private List<ItemDto> itens;
 
 }

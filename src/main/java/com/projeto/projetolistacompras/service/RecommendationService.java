135e4664e17256aafb4f5cb2be8d025189ab65c7
Rebeca <becalbuquerque_23@hotmail.com>
2025-12-05 20:06:01 -0300

    adicionando injeçao de dependencis na classe Recommendationservice

---
+++ src/main/java/com/projeto/projetolistacompras/service/RecommendationService.java
@@ -0,0 +1,9 @@
+package com.projeto.projetolistacompras.service;
+
+import com.projeto.projetolistacompras.Repository.ListaDeComprasRepository;
+
+public class RecommendationService {
+
+    private final ListaDeComprasRepository listaDeComprasRepository; 
+
+}

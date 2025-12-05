924e1327004b80109e2cb9388aaac7c1995cc5b2
Rebeca <becalbuquerque_23@hotmail.com>
2025-12-05 20:20:57 -0300

    adicionando metodo que coleta itens da lista

--- src/main/java/com/projeto/projetolistacompras/service/RecommendationService.java
+++ src/main/java/com/projeto/projetolistacompras/service/RecommendationService.java
@@ -1,6 +1,7 @@
 package com.projeto.projetolistacompras.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.projeto.projetolistacompras.Entidade.ListaDeCompras;
 import com.projeto.projetolistacompras.Repository.ListaDeComprasRepository;
@@ -14,7 +15,12 @@ public class RecommendationService {
     }
 
     public List<String> sugerirItens(String email) {
-    List<ListaDeCompras> historico = listaDeComprasRepository.findByUsuarioEmail(email);
+    List<ListaDeCompras> historico = listaDeComprasRepository.findByUsuarioEmail(email);   
+      
+
+    List<Item> historicoItens = historico.stream() 
+        .flatMap(lista -> lista.getItens().stream())
+        .collect(Collectors.toList());
     }
 
 }

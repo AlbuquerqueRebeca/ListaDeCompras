db376ff032a946921b9ff4555c54160babd4a274
Rebeca <becalbuquerque_23@hotmail.com>
2026-01-02 15:14:59 -0300

    implementando teste de integraçao para sugestoes

--- src/test/java/com/projeto/projetolistacompras/ProjetolistacomprasApplicationTests.java
+++ src/test/java/com/projeto/projetolistacompras/ProjetolistacomprasApplicationTests.java
@@ -1,6 +1,7 @@
 package com.projeto.projetolistacompras;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjetolistacomprasApplicationTests {

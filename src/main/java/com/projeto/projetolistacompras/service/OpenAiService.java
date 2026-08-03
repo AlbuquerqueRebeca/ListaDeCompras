0b263a8204419142a775a449134e55749a8c988e
AlbuquerqueRebeca <becalbuquerque_23@hotmail.com>
2026-08-03 18:20:38 -0300

    adicionando restTemplate como depedencia injetada

--- src/main/java/com/projeto/projetolistacompras/service/OpenAiService.java
+++ src/main/java/com/projeto/projetolistacompras/service/OpenAiService.java
@@ -1,12 +1,15 @@
 package com.projeto.projetolistacompras.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OpenAiService {
 
-    @value("${openai.api.key}") 
+    @Value("${openai.api.key}") 
     private String apikey;
 
-    
+    //injeção de dependencia
+    private final RestTemplate restTemplate;
 }

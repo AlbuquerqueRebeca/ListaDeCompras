b03c3bd783aeabe495746b6d1681e9f1d4ddab08
AlbuquerqueRebeca <becalbuquerque_23@hotmail.com>
2026-08-06 17:55:51 -0300

    adicionando o metodo RestTemplate

--- src/main/java/com/projeto/projetolistacompras/Config/AppConfg.java
+++ src/main/java/com/projeto/projetolistacompras/Config/AppConfg.java
@@ -1,13 +1,17 @@
 package com.projeto.projetolistacompras.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfg {
 
 
     @Bean
-    
+    public RestTemplate restTemplate(){
+        
+    }
 
 
     

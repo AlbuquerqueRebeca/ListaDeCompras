f166bc596d3da2dbe9a15918b0d12e1c5b5aa35b
AlbuquerqueRebeca <becalbuquerque_23@hotmail.com>
2026-08-31 19:08:03 -0300

    adicionando retorno para itens de usuario

--- src/main/java/com/projeto/projetolistacompras/service/OpenAiService.java
+++ src/main/java/com/projeto/projetolistacompras/service/OpenAiService.java
@@ -89,8 +89,8 @@ public class OpenAiService {
  }  
  
  public String sugerirReceita(String email){ //
- List<String itensDoUsuario = buscarItensDoUsuario(email); //buscando itens de usuario pelo email
- 
+ List<String> itensDoUsuario = buscarItensDoUsuario(email); //buscando itens de usuario pelo email
+    return gerarSugestao(itensDoUsuario); 
  }
 
 
